287bfec78c91675dd126f4316db7563c0101cc7a
Shanmukh459 <saikrrish459@gmail.com>
2024-04-18 13:20:41 -0700

    optimised event handler using useCallback hook

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ const initialMousePosition = {x: width / 2, y: height / 2}
 function App() {
   const [mousePosition, setMousePosition] = React.useState(initialMousePosition)
 
-  const handleMouseMove = (event) => {
+  const handleMouseMove = React.useCallback(event => {
     const { clientX, clientY } = event
     setMousePosition({ x: clientX, y: clientY})
-  }
+  }, [setMousePosition])
 
   return (
     <svg height={height} width={width} onMouseMove={handleMouseMove}>
