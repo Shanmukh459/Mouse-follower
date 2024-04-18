42bef48e12280b3cd53e53ab75178a5be624a4c8
Shanmukh459 <saikrrish459@gmail.com>
2024-04-18 12:59:28 -0700

    added a circle

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+const width = 960
+const height = 500
+const circleX = width / 2
+const circleY = height / 2
+const circleRadius = 30
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <svg height={height} width={width}>
+      <circle 
+        cx={circleX}
+        cy={circleY}
+        r={circleRadius}
+      />
+    </svg>
   )
 }
 
